68843f0bec900cca001e16d9a6807972db6973cc
= <=>
2025-08-22 23:38:31 +0530

    13. Roman to Integer

---
+++ arrays/13.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def romanToInt(self, s):
+        val = { 'I':1 , 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+        total = 0
+        i = 0 
+        while i < len(s):
+            if i+1 < len(s) and  val[s[i]] < val[s[i+1]]:
+                total += val[s[i+1]] - val[s[i]]
+                i += 2
+            else:
+                total += val[s[i]]
+                i += 1
+        return total
+
+
